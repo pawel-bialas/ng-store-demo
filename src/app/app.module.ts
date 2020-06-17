8e5f75d5452f03e48752f7980a4bfddc93c95ef3
pawel-bialas <zylex@tlen.pl>
2020-06-17 18:42:54 +0200

    added other components and defined routes for them

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { MainNavComponent } from './main-nav/main-nav.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 
 @NgModule({
@@ -18,7 +24,13 @@ import { ProductsComponent } from './products/products.component';
     MainNavComponent,
     HomeComponent,
     LoginComponent,
-    ProductsComponent
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent
   ],
   imports: [
     BrowserModule,
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,10 +2,24 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
 import {ProductsComponent} from './products/products.component';
+import {MyOrdersComponent} from './my-orders/my-orders.component';
+import {ShoppingCartComponent} from './shopping-cart/shopping-cart.component';
+import {CheckOutComponent} from './check-out/check-out.component';
+import {OrderSuccessComponent} from './order-success/order-success.component';
+import {AdminProductsComponent} from './admin/admin-products/admin-products.component';
+import {AdminOrdersComponent} from './admin/admin-orders/admin-orders.component';
 
 
 export const ROUTES: Routes = [
+
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'products', component: ProductsComponent},
+  {path: 'my-orders', component: MyOrdersComponent},
+  {path: 'my-cart', component: ShoppingCartComponent},
+  {path: 'check-out', component: CheckOutComponent},
+  {path: 'order-success', component: OrderSuccessComponent},
+
+  {path: 'admin-products', component: AdminProductsComponent},
+  {path: 'admin-orders', component: AdminOrdersComponent},
 ];
