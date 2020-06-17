f6ce4db76f8d194c14722d1bde63e6b160005cab
pawel-bialas <zylex@tlen.pl>
2020-06-18 00:06:54 +0200

    added deployment script to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBuwynH9ekoPY7S-O0nWN4k-oV1YOm2z9E',
-    authDomain: 'my-store-demo-6b637.firebaseapp.com',
-    databaseURL: 'https://my-store-demo-6b637.firebaseio.com',
-    projectId: 'my-store-demo-6b637',
-    storageBucket: 'my-store-demo-6b637.appspot.com',
-    messagingSenderId: '85489549214',
-    appId: '1:85489549214:web:608ffb32d9bbd1272c608f'
+    apiKey: 'AIzaSyDWP-6YUdJPwGHxnWi9LfmdmROj8pA75J0',
+    authDomain: 'ng-sample-store.firebaseapp.com',
+    databaseURL: 'https://ng-sample-store.firebaseio.com',
+    projectId: 'ng-sample-store',
+    storageBucket: 'ng-sample-store.appspot.com',
+    messagingSenderId: '413629279656',
+    appId: '1:413629279656:web:fdebc94b19f034e828019d'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBuwynH9ekoPY7S-O0nWN4k-oV1YOm2z9E',
-    authDomain: 'my-store-demo-6b637.firebaseapp.com',
-    databaseURL: 'https://my-store-demo-6b637.firebaseio.com',
-    projectId: 'my-store-demo-6b637',
-    storageBucket: 'my-store-demo-6b637.appspot.com',
-    messagingSenderId: '85489549214',
-    appId: '1:85489549214:web:608ffb32d9bbd1272c608f'
+    apiKey: 'AIzaSyDWP-6YUdJPwGHxnWi9LfmdmROj8pA75J0',
+    authDomain: 'ng-sample-store.firebaseapp.com',
+    databaseURL: 'https://ng-sample-store.firebaseio.com',
+    projectId: 'ng-sample-store',
+    storageBucket: 'ng-sample-store.appspot.com',
+    messagingSenderId: '413629279656',
+    appId: '1:413629279656:web:fdebc94b19f034e828019d'
   }
 };
 
