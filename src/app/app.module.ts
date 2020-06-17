549d773772c995af4a69b3f5d93219961e400a53
pawel-bialas <zylex@tlen.pl>
2020-06-17 13:44:19 +0200

    saome modifications with scripts array in angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {RouterModule} from '@angular/router';
+import {ROUTES} from './routes';
+
 
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { HomeComponent } from './home/home.component';
-import {ROUTES} from './routes';
+import { LoginComponent } from './login/login.component';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,7 +1,11 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {ProductsComponent} from './products/products.component';
 
 
 export const ROUTES: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'products', component: ProductsComponent},
 ];
